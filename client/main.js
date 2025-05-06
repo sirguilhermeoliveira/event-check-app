152ed8f9e49d183a46cd4ffd33d6a61bd5bdb70a
root <root@RECO-IMPERADOR>
2025-05-06 20:44:46 -0300

    refactor: changed to new home import

--- client/main.js
+++ client/main.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createRoot } from 'react-dom/client';
-import { App } from '../ui/App';
+import { Home } from '../ui/pages/home/index';
 
 Meteor.startup(() => {
   const root = createRoot(document.getElementById('app'));
-  root.render(<App />);
+  root.render(<Home />);
 });
