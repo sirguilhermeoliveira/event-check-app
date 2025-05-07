ddee5c0292b440bce5777902cd5a5d627b2e17c4
root <root@RECO-IMPERADOR>
2025-05-07 02:00:58 -0300

    refactor: correct event summary logic for checked by company list

--- ui/components/event-summary/index.js
+++ ui/components/event-summary/index.js
@@ -1,30 +1,40 @@
 import React from 'react';
 
-export const EventSummary = ({ checkedInCount, notCheckedInCount, companyBreakdown }) => (
+export const EventSummary = ({ checkedInCount, notCheckedInCount, companyBreakdown }) => {
+  const groupedCompanies = companyBreakdown?.reduce((acc, curr) => {
+    const name = curr.companyName || 'No associated company';
+    acc[name] = (acc[name] || 0) + 1;
+    return acc;
+  }, {});
+
+  return (
     <div className="mt-4 p-4 border rounded flex flex-col items-center justify-center">
       <div>
-
-      <h2 className="font-bold text-lg mb-4 text-center p-[5px]">Event Summary</h2>
-      <div>
-    <div className="flex flex-row">
-      <div className="font-bold">🟢 Checked-in: </div><p>{checkedInCount}</p>
-    </div>
-    <div className="flex flex-row">
-      <div className="font-bold">🔴 Not checked-in yet: </div><p> {notCheckedInCount}</p>
-      </div>
-      </div>
-      {checkedInCount > 0 && (
-        <div className="p-1 justify-center items-center">
-          <p className="font-bold ">🏢 Checked-in by company:</p>
-          <ul>
-            {Object.entries(companyBreakdown).map(([company, count]) => (
-              <li key={company}>
-                ⭐ {company}: {count}
-              </li>
-            ))}
-          </ul>
+        <h2 className="font-bold text-lg mb-4 text-center p-[5px]">Event Summary</h2>
+        <div>
+          <div className="flex flex-row">
+            <div className="font-bold">🟢 Checked-in: </div>
+            <p>{checkedInCount >= 0 ? checkedInCount : 'N/A'}</p>
+          </div>
+          <div className="flex flex-row">
+            <div className="font-bold">🔴 Not checked-in yet: </div>
+            <p>{notCheckedInCount >= 0 ? notCheckedInCount : 'N/A'}</p>
+          </div>
         </div>
-      )}
+
+        {checkedInCount > 0 && groupedCompanies && (
+          <div className="p-1 justify-center items-center">
+            <p className="font-bold">🏢 Checked-in by company:</p>
+            <ul>
+              {Object.entries(groupedCompanies).map(([companyName, count]) => (
+                <li key={companyName}>
+                  ⭐ {companyName}: {count}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
-      </div>
+    </div>
   );
+};
