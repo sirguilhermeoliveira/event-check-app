189d802f5e74562841af567a5b6c79b80c08136b
root <root@RECO-IMPERADOR>
2025-05-07 02:00:28 -0300

    feat: new component to handle error msgs from api

---
+++ ui/components/no-people-found/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const NoPeopleFound = ({events, people, selectedEventId}) => (
+  <div className="flex items-center justify-center h-screen">
+    {!events && !people ? "No events or people found" :
+    <div>
+    <span className="text-2xl font-semibold text-gray-800">{!events || events.length === 0 && "No events found"}</span>
+    <span className="text-2xl font-semibold text-gray-800">{!people && selectedEventId && "No people found for this event"}</span>
+    <span className="text-2xl font-semibold text-gray-800">{!selectedEventId && events.length > 0 && "Select an event above"}</span>
+    <span className="text-2xl font-semibold text-gray-800">{people?.length === 0 && selectedEventId && "No results found"}</span>
+    </div>
+    }
+  </div>
+);
