5ca13615aed417451d11211d35dafc706e458bcc
root <root@RECO-IMPERADOR>
2025-05-06 23:58:08 -0300

    chore: update tailwind to use on components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./ui/pages/**/*.js', './client/*.html'],
+  content: ['./ui/pages/**/*.js', './client/*.html', './ui/components/**/*.js'],
   theme: {
     extend: {},
   },
