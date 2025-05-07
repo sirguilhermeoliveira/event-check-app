61c7c6ff435214af98537c5eb30e39d1e548549c
root <root@RECO-IMPERADOR>
2025-05-07 01:57:12 -0300

    refactor: correct schemas for people and communities

--- api/collections/communities/communities.js
+++ api/collections/communities/communities.js
@@ -1,3 +1,8 @@
 import { Mongo } from 'meteor/mongo';
 
 export const Communities = new Mongo.Collection('communities');
+
+Communities.schema = {
+    _id: { type: String, optional: false },
+    name: { type: String, optional: false },
+  };
