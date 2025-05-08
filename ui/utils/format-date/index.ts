ef539578f7f43eeb5fe66b782396814614893f6b
root <root@RECO-IMPERADOR>
2025-05-08 11:54:26 -0300

    fix: correct types for formatDate

--- ui/utils/format-date/index.ts
+++ ui/utils/format-date/index.ts
@@ -1,12 +1,5 @@
-export const formatDate = (date: string | null | undefined): string => {
-  if (!date || (typeof date !== 'string')) {
-    return 'N/A';
-  }
-  return new Date(date).toLocaleString('en-US', {
-        month: '2-digit',
-        day: '2-digit',
-        year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-      })
+export const formatDate = (date?: string | Date | null | undefined): string => {
+  if (!date) return 'N/A';
+  const parsedDate = typeof date === 'string' ? new Date(date) : date;
+  return parsedDate.toLocaleDateString();
 };
